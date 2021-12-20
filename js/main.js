42580a41f9d5867df0a66c878c3d4a2bd1597c21
Ahmadsabili <ahmadsabili0081@gmail.com>
2021-12-21 02:51:02 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+AOS.init({
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+const btn = document.querySelector('.btn');
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', scrollY > 0);
+    btn.classList.toggle('btnShow', scrollY > 0);
+});
+btn.addEventListener('click', btnClick);
+function btnClick(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('showNav');
+    hamburger.classList.toggle('rotate');
+});
